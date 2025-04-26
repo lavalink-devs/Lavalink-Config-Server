78c18b48f8bb7d3a11c751739006789dc071ad3d
infnibor <156368035+infnibor@users.noreply.github.com>
2025-04-27 00:25:42 +0200

    Add Lavalink Config Server Page (Enpoint /)

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot:3.3.0")
     implementation("org.springframework.cloud:spring-cloud-config-server:4.2.2")
     implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.3.0")
 }
 
 fun versionFromGit(): String {
