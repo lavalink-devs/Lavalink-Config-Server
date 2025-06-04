4e109cc99f4dae00e47c32c4cd51ce036585fcae
topi314 <git@topi.wtf>
2025-06-04 23:02:30 +0200

    fix not working http basic auth

--- src/main/java/dev/lavalink/config/server/SecurityConfig.java
+++ src/main/java/dev/lavalink/config/server/SecurityConfig.java
@@ -2,6 +2,7 @@ package dev.lavalink.config.server;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
             .formLogin(form -> form.loginPage("/login").permitAll())
+            .httpBasic(Customizer.withDefaults())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/style.css", "/lavalink.svg").permitAll()
                 .anyRequest().authenticated())
